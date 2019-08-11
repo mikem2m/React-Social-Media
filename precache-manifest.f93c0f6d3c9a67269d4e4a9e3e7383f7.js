a29f97fdb27ba73f48ecbad2f07ff6250bad2e2b
mikem2m <michael08ace@gmail.com>
2019-08-12 02:03:09 +0800

    Updates

---
+++ precache-manifest.f93c0f6d3c9a67269d4e4a9e3e7383f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/React-Social-Media/static/media/noProfile.b6681619.png"
+  },
+  {
+    "revision": "6eb2870a3a2dcfe141fc",
+    "url": "/React-Social-Media/static/js/runtime~main.4bdee530.js"
+  },
+  {
+    "revision": "3532c3df6cd12d148878",
+    "url": "/React-Social-Media/static/js/main.b978f99e.chunk.js"
+  },
+  {
+    "revision": "42e606003ba4297a6b9c",
+    "url": "/React-Social-Media/static/js/2.4a3e59de.chunk.js"
+  },
+  {
+    "revision": "3532c3df6cd12d148878",
+    "url": "/React-Social-Media/static/css/main.f05debd5.chunk.css"
+  },
+  {
+    "revision": "ace26ce7d9e0c6a6664b5091e30ff3e8",
+    "url": "/React-Social-Media/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7841add667903be3f011e2e80a4469c0.js"
+  "/React-Social-Media/precache-manifest.f93c0f6d3c9a67269d4e4a9e3e7383f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Social-Media/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
